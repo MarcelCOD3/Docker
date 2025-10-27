638d93a2ec9a91e0f67d69307233ae34eca48923
MizCODE <marcelbarriosmarchisio@gmail.com>
2025-10-27 13:56:33 -0300

    Update database connection parameters

--- config/database.php
+++ config/database.php
@@ -1,11 +1,16 @@
 <?php
 try {
     $db = new PDO(
-        'mysql:host=db;dbname=draftosaurus_db;charset=utf8',
-        'root',
-        '' // sin contraseña
+        'mysql:host=database;dbname=draftosaurus_db;charset=utf8', // nombre del servicio correcto
+        'root',        // usuario de Docker
+        'drafto123'    // contraseña de Docker
     );
+
+    // Establecer modo de error a excepción
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // (opcional) mensaje de éxito
+    // echo "Conexión exitosa a la base de datos";
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
